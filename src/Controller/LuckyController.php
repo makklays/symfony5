09b219f3f0a9e4483403bb365c7f58069e91266b
Alexander <alexander@makklays.com.ua>
2021-03-31 17:30:11 +0200

    first commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function randomNumber() : Response
+    {
+        $num = random_int(0, 350);
+
+        return new Response('<html><body>Fatima\'s lucky number:' . $num . '</body></html>');
+    }
+}
+
+
