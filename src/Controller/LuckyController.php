b7d2b7c574ae70a7e9524e23148da08e82059acb
Alexander <alexander@makklays.com.ua>
2022-03-11 16:57:07 +0100

    2022-03-11 Add pages 'Lucky'

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,14 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
+    public function index(): Response 
+    {
+        return $this->render('lucky/index.html.twig');
+    }
+
     public function randomNumber() : Response
     {
         $num = random_int(0, 350);
 
-        return new Response('<html><body>Fatima\'s lucky number:' . $num . '</body></html>');
+        //return new Response('<html><body>Fatima\'s lucky number:' . $num . '</body></html>');
+
+        return $this->render('lucky/number.html.twig', [
+            'page_title' => 'Lucky number',
+            'num' => $num,
+        ]);
+    }
+
+    public function randomFrase(): Response
+    {
+        $frase = random_int(111, 999);
+
+        return $this->render('lucky/frase.html.twig', [
+            'page_title' => 'Lucky prase',
+            'frase' => $frase,
+        ]);
     }
 }
 
